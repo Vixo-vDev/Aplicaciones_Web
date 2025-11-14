e82d74a045e165930867170072bd35b8cd690932
David Valenzuela Guijosa <20243ds046@utez.edu.mx>
2025-11-14 14:10:48 -0600

    .

---
+++ prueba/src/main/java/com/example/prueba/HolaController.java
@@ -0,0 +1,27 @@
+package com.example.prueba;
+
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HolaController {
+
+    @PostMapping(
+            path = "/saludo",
+            // enviar informacion en formato JSON
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            // devuelve la info en formato JSON
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Map<String, String> saludar(@RequestBody Map<String, String> datos) {
+        String nombre = datos.get("nombre");
+        String apellido = datos.get("apellido");
+        String mensaje = "Hola " + nombre + " " + apellido + " bienvenido a springboot y top tops";
+        return Map.of("mensaje", mensaje);
+    }
+}
